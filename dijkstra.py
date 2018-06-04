365540ef3e8ddc0b442403d714f40c46b648f51b
Chernyshov <xpom55@mail.ru>
2018-06-04 21:21:38 +0300

    init Algorithm class with calculateShortestPath and getShortestPathTo

--- dijkstra.py
+++ dijkstra.py
@@ -3,10 +3,10 @@ import heapq
 
 class Edge(object):
 
-  def __init__(self, weight, startVertex, tragetVertex):
+  def __init__(self, weight, startVertex, targetVertex):
     self.weight = weight
     self.startVertex = startVertex
-    self.tragetVertex = tragetVertex
+    self.targetVertex = targetVertex
 
 class Node(object):
 
@@ -26,3 +26,34 @@ class Node(object):
     selfPriority = self.minDistance
     otherPriority = other.minDistance
     return selfPriority < otherPriority
+
+class Algorithm(object):
+
+  def calculateShortestPath(self, vertexList, startVertex):
+
+    queue = []
+    startVertex.minDistance = 0
+    heapq.heappush(queue, startVertex) # init heap in 'q'
+
+    while len(queue) > 0:
+      acturalVertex = heapq.heappop(queue) # get node with min distance
+
+      for edge in acturalVertex.adjacenciesList:
+        u = edge.startVertex
+        v = edge.targetVertex
+        newDistance = u.minDistance + edge.weight
+
+        if newDistance < v.minDistance:
+          v.predecessor = u
+          v.minDistance = newDistance
+          heapq.heappush(queue, v)
+
+  def getShortestPathTo(self, targetVertex)
+    print('Shortest path to vertex')
+
+    node = targetVertex
+
+    while node is not None:
+      print('%s ' % node.name)
+      node = node.predecessor
+
